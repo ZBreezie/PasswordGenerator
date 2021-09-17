5c45e83fcf834c2cf79bef862d37ee233822502d
ZBreezie <zpmc55@yahoo.com>
2021-09-17 18:57:40 -0400

    added exits to functions + uppercase/lowercase bugfix

--- script.js
+++ script.js
@@ -13,24 +13,22 @@ function writePassword() {
 
 
 
-
-
-
-//generate password
+//generate password function
 function generatePassword() {
   
   
   //evaluate password length
-  var length = Number(prompt("How many characters will your password be? Please enter a number between 8 and 128"));
-  if (length < 8 || length > 128) {
+  var length = Number(prompt("How many characters will your password be? Please enter a number between 8 and 128!"));
+
+  //if invalid number is given, or window is cancelled, force TryAgain
+  if (length < 8 || length > 128 || length == false) {
     alert("Please choose a value between 8 and 128!")
     var TryAgain = "Try again!";
     return TryAgain
   }
   
   //"empty" string for characters to lay
-  var charSet;
-
+  var charSet = "";
 
   //ask for character types
   if (window.confirm("Would you like uppercase letters in your password?")) {
@@ -45,14 +43,17 @@ function generatePassword() {
   if (window.confirm("Would you like symbols in your password?")) {
     charSet = charSet + "!@#$%^&*()_+=-<>?,./:[]{}";
   } 
-  if (charSet = undefined) {
+
+  
+  //if no options are chosen, force TryAgain
+  if (charSet == "") {
     alert("You didnt choose any options! Please try again")
     var TryAgain = "Try again!";
     return TryAgain
   }
   
 
-  //return value
+  //return the value
   var retVal = "";
   for (var i = 0; i < length; i++) {
     //picks a character within charSet at index of random number
@@ -60,6 +61,9 @@ function generatePassword() {
   }
   return retVal;
 }
+
+
+
 
 
 // Add event listener to generate button
