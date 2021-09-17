7661b02a9cb407fba77c98fdbfebb5a7c66c20fa
ZBreezie <zpmc55@yahoo.com>
2021-09-17 18:32:31 -0400

    basic logic

--- script.js
+++ script.js
@@ -7,8 +7,60 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
+
+
+
+
+
+
+//generate password
+function generatePassword() {
+  
+  
+  //evaluate password length
+  var length = Number(prompt("How many characters will your password be? Please enter a number between 8 and 128"));
+  if (length < 8 || length > 128) {
+    alert("Please choose a value between 8 and 128!")
+    var TryAgain = "Try again!";
+    return TryAgain
+  }
+  
+  //"empty" string for characters to lay
+  var charSet;
+
+
+  //ask for character types
+  if (window.confirm("Would you like uppercase letters in your password?")) {
+    charSet = charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (window.confirm("Would you like lowercase letters in your password?")) {
+      charSet = charSet + "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (window.confirm("Would you like numbers in your password?")) {
+    charSet = charSet + "0123456789";
+  }
+  if (window.confirm("Would you like symbols in your password?")) {
+    charSet = charSet + "!@#$%^&*()_+=-<>?,./:[]{}";
+  } 
+  if (charSet = undefined) {
+    alert("You didnt choose any options! Please try again")
+    var TryAgain = "Try again!";
+    return TryAgain
+  }
+  
+
+  //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
